f14b6a054bbdf71d9a552cfc5763a85d1a6f1329
MarilyneIkupu18 <ikupumarilyne18@gmail.com>
2026-04-21 10:09:09 +1000

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -15,6 +16,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LBSQB6R7MZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LBSQB6R7MZ');
+          `}
+        </Script>
+      </head>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
